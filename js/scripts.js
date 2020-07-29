a7ded8cf949b5297309c4189049c96c7769ebddd
Chris VP <cvpacupuncture@gmail.com>
2020-07-28 21:38:12 -0700

    make 2 functions and add conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
-
 // Business logic:
+function makeRange(maxNumber) {
+  let range = [];
+  for (i = 0; i <= maxNumber; i++) {
+      range.push(i);
+  }
+  return range;
+}
 
+function robotResults(rangeNumbers){
+  let finalArray = [];
+  rangeNumbers.forEach(function(number) {
+    if (number.toString().includes("3")) {
+      finalArray.push("Won't you be my neighbor?");
+    }
+    else if (number.toString().includes("2")) {
+      finalArray.push("Boop!");
+    }
+    else if (number.toString().includes("1")) {
+      finalArray.push("Beep!");
+    }
+    else {
+      finalArray.push(number);
+    }
+  });
+  return finalArray; 
+
+}
 
 
 
@@ -9,21 +34,13 @@
 $(document).ready(function() {
   $("#initialQ").submit(function(event) {
     event.preventDefault();
-
     const maxNumber = $("#rangeInput").val();
-    
-    let numbers = [0];
-    for (i = 0; i < maxNumber; i++) {
-        numbers[i] = i;
-    }
-    
+    let rangeNumbers = makeRange(maxNumber);
+    let result = robotResults(rangeNumbers);
+$("#response").show();
+$("#commence").slideDown(2000);
+$("#lastLine").show(6000);
+$("#answer").text(result);
 
-
-    $("#response").show();
-    $("#answer").text(numbers);
-    alert(numbers);
-    alert(maxNumber);
-  });
 });
-
-
+});
